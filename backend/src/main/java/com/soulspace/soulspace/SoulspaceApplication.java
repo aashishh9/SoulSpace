76791414b7aab9e7049be377429b9b15a76749d9
aashishh9 <ashishpawar1776@gmail.com>
2026-02-19 12:31:40 +0530

    Initial backend setup

---
+++ backend/src/main/java/com/soulspace/soulspace/SoulspaceApplication.java
@@ -0,0 +1,13 @@
+package com.soulspace.soulspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulspaceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/soulspace/soulspace/SoulspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulspace.soulspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
